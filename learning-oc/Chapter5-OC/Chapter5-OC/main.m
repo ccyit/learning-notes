e456ab41dfab64ecbb3f7477178298aa4aecc7f9
ccyag <22377832@qq.com>
2019-10-30 19:05:14 +0800

    add fiels

---
+++ learning-oc/Chapter5-OC/Chapter5-OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Chapter5-OC
+//
+//  Created by ccyag on 30/10/2019.
+//  Copyright © 2019 com.ccyag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- learning-oc/MyFirstProject/MyFirstProject/main.m
+++ learning-oc/MyFirstProject/MyFirstProject/main.m
@@ -1,17 +1,44 @@
-//
-//  main.m
-//  MyFirstProject
-//
-//  Created by ccyag on 30/10/2019.
-//  Copyright © 2019 com.ccyag. All rights reserved.
-//
+ 
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        // NSLog(@"Hello, World!");
+        int randomNumber = 1;
+        int userGuess = 1;
+        BOOL continueGuessing = YES;
+        BOOL keepPlaying = YES;
+        char yesNo = ' ';
+        while (keepPlaying) {
+            randomNumber = arc4random() % 101;
+            NSLog(@"The random number to guess is:%d", randomNumber);
+            while (continueGuessing) {
+                NSLog(@"Pick a number between 0 and 100:");
+                
+                scanf("%d", &userGuess);
+
+               
+                fgetc(stdin);
+                if(userGuess == randomNumber){
+                    continueGuessing = NO;
+                    NSLog(@"Correct number!");
+                }
+                else if(userGuess > randomNumber){
+                    NSLog(@"Your guess is too high.");
+                } else {
+                    NSLog(@"Your guess is too low.");
+                }
+                NSLog(@"The user guessed %d.", userGuess);
+            }
+            NSLog(@"Play Again? Y or N:");
+            yesNo = fgetc(stdin);
+            if (yesNo == 'N' || yesNo == 'n') {
+                keepPlaying = NO;
+            }
+            continueGuessing = YES;
+        }
     }
     return 0;
 }
